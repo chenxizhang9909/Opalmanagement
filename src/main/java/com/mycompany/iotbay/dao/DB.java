3e5f9dbbf659696e333377050fe045af56fe7c3e
Estella <89052839+EstellaCheng42@users.noreply.github.com>
2021-09-29 01:48:15 +0800

    setup database

--- src/main/java/com/mycompany/iotbay/dao/DB.java
+++ src/main/java/com/mycompany/iotbay/dao/DB.java
@@ -12,9 +12,9 @@ import java.sql.Connection;
  */
 public abstract class DB {
     protected String URL = "jdbc:derby://localhost:1527/";//replace this string with your jdbc:derby local host url   
-    protected String db = "IoTBay";//name of the database   
+    protected String db = "opalcard";//name of the database   
     protected String dbuser = "root";//db root user   
-    protected String dbpass = "12345678"; //db root password   
+    protected String dbpass = "0000"; //db root password   
     protected String driver = "org.apache.derby.jdbc.ClientDriver"; //jdbc client driver - built in with NetBeans   
     protected Connection conn; //connection null-instance to be initialized in sub-classes
 }
